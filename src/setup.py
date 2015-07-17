84ccc9725e5ee8870db08a2a2a5fb6eb6a7a60a3
Koby Aharon <kobyahron@gmail.com>
2015-07-17 13:14:02 +0300

    Add setup.py file

---
+++ src/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "python_distro",
+    version = "0.0.1",
+    author = "Koby Aharon",
+    author_email = "kobyahron@gmail.com",
+    description = ("An demonstration of how to create python distribution."),        
+    packages=['python'],        
+)
